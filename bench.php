3571f406184572aef11f6854e2153ff80fc67cfd
Arnaud Héritier <aheritier@exoplatform.com>
2012-09-20 15:01:08 +0200

    Fix images

--- bench.php
+++ bench.php
@@ -57,50 +57,46 @@ function getImagesList ($directory) {
           </div>
           <div class="row-fluid">
             <div class="span10 offset1">
+              <?php
+              $images = getImagesList($_SERVER['BENCHMARKS_DIR']."/".$benchmark."/jmeter-results");
+              foreach( $images as $image) {
+                ?>
               <div class="row-fluid">
                 <div class="span4"></div>
                 <div class="span8">
                   <ul class="thumbnails">
-                    <?php
-                    $images = getImagesList($_SERVER['BENCHMARKS_DIR']."/".$benchmark."/jmeter-results");
-                    foreach( $images as $image) {
-                      ?>
                     <li class="span8">
                       <div class="thumbnail">
-                        <a data-toggle="modal" href='<?="#".$image?>'> <img src="https://qaf-reports.exoplatform.org/archives/gateinuxp/<?=$benchmark?>/jmeter-results/<?=$image?>" alt="" />
-                        </a>
+                        <img src="https://qaf-reports.exoplatform.org/archives/gateinuxp/<?=$benchmark?>/jmeter-results/<?=$image?>" alt="" />
                         <h3>
                           <?=$image?>
                         </h3>
-                        <p>Thumbnail caption...</p>
+                        <p><button type="button" data-toggle="modal" data-target="#<?=$image?>">Enlarge ...</button></p>
                       </div>
                     </li>
-                    <div class="modal fade" id="<?=$image?>">
-                      <div class="modal-header">
-                        <a class="close" data-dismiss="modal">x</a>
-                        <h3>
-                          <?=$image?>
-                        </h3>
-                      </div>
-                      <div class="modal-body">
-                        <div class="row-fluid">
-                          <ul class="thumbnails">
-                            <li class="span12">
-                              <div class="thumbnail">
-                                <img src="https://qaf-reports.exoplatform.org/archives/gateinuxp/<?=$benchmark?>/jmeter-results/<?=$image?>" alt="" />
-                              </div>
-                            </li>
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                    <?php
-                    }
-                    ?>
                   </ul>
                 </div>
               </div>
             </div>
+            <div class="modal fade" id="<?=$image?>" tabindex="-1" role="dialog" aria-labelledby="<?=$image?>-label" aria-hidden="true">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h3 id="<?=$image?>-label">
+                  <?=$image?>
+                </h3>
+              </div>
+              <div class="modal-body">
+                <p>
+                  <img src="https://qaf-reports.exoplatform.org/archives/gateinuxp/<?=$benchmark?>/jmeter-results/<?=$image?>" alt="" />
+                </p>
+              </div>
+              <div class="modal-footer">
+                <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+              </div>
+            </div>
+            <?php
+              }
+              ?>
           </div>
         </div>
       </div>
